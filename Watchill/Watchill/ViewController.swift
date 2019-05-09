7c3be0fb8d51f1cc47f84eeef4c0988c0002123f
Danilo da Rocha Lira Araujo <drla@10-48-48-120.0200.ufpe.br.iosda.org>
2019-05-09 11:32:06 -0300

    Criação do projeto

---
+++ Watchill/Watchill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Watchill
+//
+//  Created by Danilo da Rocha Lira Araujo on 09/05/19.
+//  Copyright © 2019 Danilo da Rocha Lira Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
